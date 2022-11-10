5ec083fbe03d4d6342f3059ec0baed956487e83e
Tim D <timd@optixgate.com>
2022-11-10 13:02:39 -0500

    new script

---
+++ script.ps1
@@ -0,0 +1,4 @@
+$var - Get-V[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    <#Category#>'PSProvideDefaultParameterValue', <#CheckId>$null, Scope='Function',
+    Justification = 'Reason for suppressing'
+)]

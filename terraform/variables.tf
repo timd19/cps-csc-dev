ff6caf07abd52463732c3f6e37b1fe16ccb60a21
Tim D <timd@optixgate.com>
2022-12-09 19:36:30 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,23 @@ variable "location" {
   description = "Location to deploy the resource group"
   default     = "West US 2"
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "subscription_id"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "tenant_id"
+}
+
+variable "client_id" {
+  type        = string
+  description = "client_id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "client_secret"
+}

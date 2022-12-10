65c2ad8ecf31ea3068eb362b1da758e137fe1332
Tim D <timd@optixgate.com>
2022-12-09 19:34:59 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,4 +10,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
